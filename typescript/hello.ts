b8e5d0e6923714443a07e8a266e067ed35f61457
Jeevan s <sathishjeevan2004@gmail.com>
2026-08-12 14:33:15 +0530

    working with nested object using type in typescript

--- typescript/hello.ts
+++ typescript/hello.ts
@@ -1,7 +1,44 @@
-const array: number[] = [10, 2, 3, 4, 5];
+// const array: number[] = [10, 2, 3, 4, 5];
 
-for (let i: number = 0; i < array.length; i++) {
-  console.log(array[i]);
-}
+// for (let i: number = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
 
+// type User = {
+//   name: string;
+//   age: number;
+// };
+// const user: User = {
+//   name: "jeevan",
+//   age: 20,
+// };
+// user.name = "sathish";
+
+// console.log(Object.values(user));
+
+// const User1: User = {
+//   name: "prajwal",
+//   age: 20,
+// };
+
+// console.log(User1.name);
+type Adress = {
+  city: string;
+};
+
+type User = {
+  name: string;
+  age: number;
+  adress: Adress;
+};
+
+const info: User = {
+  name: "jeevan",
+  age: 20,
+  adress: {
+    city: "mysore",
+  },
+};
+info.adress.city = "mandya";
+console.log(info);
 export {};
